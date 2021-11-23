f58528deeea92df655f5783a4e03813af2133cfa
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-23 21:03:03 +0800

    BIKE-#2: Improve the OpenAPI sample docs, add security integration

--- src/main/java/com/bike/app/api/UserAPI.java
+++ src/main/java/com/bike/app/api/UserAPI.java
@@ -1,11 +1,14 @@
 package com.bike.app.api;
 
 import com.bike.app.core.usecase.CreateUser;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -19,24 +22,34 @@ public class UserAPI {
 
     private final CreateUser createUser;
 
-    @Schema(name = "CreateUser")
-    @Data
-    @RequiredArgsConstructor
-    @AllArgsConstructor
+    @Schema(name = "UserCreateRequest", description = "A new system user")
+    @Data @RequiredArgsConstructor @AllArgsConstructor
     public static class CreateUserRequest {
+
+        @Schema(description = "Unique username for login", example = "admin")
         String username;
+
+        @Schema(description = "User secret", example = "admin123")
         String password;
+
+        @Schema(description = "The name that's displayed in the UI", example = "Administrator")
         String displayName;
     }
 
     @Value
+    @Schema(name = "UserCreateResponse", description = "the system id")
     public static class CreatedUserResponse {
-        String userId;
+
+        @Schema(description = "System-generated, unique id", example = "1d5313be-713f-4650-bee3-2d0a18ffaa65")
+        String id;
     }
 
-    @Post(uri = "/")
-    @Operation()
+    @Post(uri = "/", produces = MediaType.APPLICATION_JSON)
     @RolesAllowed({"ADMIN"})
+    @Operation(operationId = "user-create", summary = "Create a new User", description = "Allows administrators to create a new User for the system")
+    @ApiResponse(responseCode = "401", description = "Authentication is required")
+    @ApiResponse(responseCode = "403", description = "User is not allowed to create a new user")
+    @Tag(name = "User")
     public CreatedUserResponse create(@Body CreateUserRequest createUserRequest) {
         var createUser = new CreateUser.UserCreateCommand()
                 .withUsername(createUserRequest.username)
--- src/main/java/com/bike/app/framework/openapi/OpenAPIConfiguration.java
+++ src/main/java/com/bike/app/framework/openapi/OpenAPIConfiguration.java
@@ -2,9 +2,14 @@ package com.bike.app.framework.openapi;
 
 import io.micronaut.openapi.annotation.OpenAPIInclude;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -13,13 +18,25 @@ import io.swagger.v3.oas.annotations.info.License;
                 description = "a simple CRUD web application for a bike rental shop",
                 license = @License(name = "Apache 2.0", url = "https://codefactory.dev"),
                 contact = @Contact(url = "https://bikerental.com", name = "LM Bibera", email = "secret.transaction@gmail.com")
-        )
+        ),
+        security = @SecurityRequirement(name = "api", scopes = {"ADMIN", "STAFF"}),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Development Server"),
+                @Server(url = "https://api.bike.com", description = "Production Server")
+        }
 )
 @OpenAPIInclude(
         classes = {
                 io.micronaut.security.endpoints.LoginController.class,
                 io.micronaut.security.endpoints.LogoutController.class
-        }
+        },
+        tags = @Tag(name = "Security")
+)
+@SecurityScheme(
+        name = "api",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP
 )
 public class OpenAPIConfiguration {
 }

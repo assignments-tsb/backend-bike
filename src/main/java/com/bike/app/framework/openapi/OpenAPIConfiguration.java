44653cd2b25f8e55029bcf6750c5bc165feef9eb
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-23 20:17:06 +0800

    BIKE-#2: Initial attempt at generating a working OpenAPI documentation

--- src/main/java/com/bike/app/framework/openapi/OpenAPIConfiguration.java
+++ src/main/java/com/bike/app/framework/openapi/OpenAPIConfiguration.java
@@ -1,5 +1,6 @@
 package com.bike.app.framework.openapi;
 
+import io.micronaut.openapi.annotation.OpenAPIInclude;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -9,10 +10,16 @@ import io.swagger.v3.oas.annotations.info.License;
         info = @Info(
                 title = "Bike Rental Service API",
                 version = "1.0",
-                description = "a simple CRUD web applica<on for a bike rental shop",
+                description = "a simple CRUD web application for a bike rental shop",
                 license = @License(name = "Apache 2.0", url = "https://codefactory.dev"),
                 contact = @Contact(url = "https://bikerental.com", name = "LM Bibera", email = "secret.transaction@gmail.com")
         )
+)
+@OpenAPIInclude(
+        classes = {
+                io.micronaut.security.endpoints.LoginController.class,
+                io.micronaut.security.endpoints.LogoutController.class
+        }
 )
 public class OpenAPIConfiguration {
 }
